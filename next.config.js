4df19f189836cd3415dd612e067c4adc80e97cd4
Syed-Hasnain-Askari <hasnainaskari32@gmail.com>
2023-05-30 23:52:22 +0500

    change in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-    },
-    trailingSlash: true,
-    distDir: 'build',
-    optimizeFonts: false,
-}
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+	},
+	trailingSlash: true,
+	optimizeFonts: false,
+};
